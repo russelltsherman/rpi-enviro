1a3a4b43b2f4b5a299d3919b9588df0b27c368f6
Russell <russell@union-pos.com>
2019-12-22 18:41:00 -0600

    add examples

---
+++ examples/temperature.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import time
+
+from envirophat import weather, leds
+
+
+print("""Light the LEDs upon temperature increase.
+
+Press Ctrl+C to exit.
+
+""")
+
+threshold = None
+
+try:
+    while True:
+        temperature = weather.temperature()
+
+        if threshold is None:
+            threshold = temperature + 2
+
+        print("{} degrees Celsius".format(temperature))
+        if temperature > threshold:
+            leds.on()
+        else:
+            leds.off()
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
